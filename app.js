d3639a96eabfb6619a8e3036c3057aa3e1afa187
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-29 19:21:57 +0600

    Round Score & Initial Dice

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+var scores, roundScore, activePlayer, dice;
+scores = [0,0];
+roundScore = 0;
+activePlayer =1;
+
+dice =  Math.floor(Math.random()*6)+1;
+
+//setter
+document.querySelector('#current-'+ activePlayer).textContent = dice;
+// document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>'; 
+
+
+//getter //read from html
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+//initial dice
+
+document.querySelector('.dice').style.display = 'none';
