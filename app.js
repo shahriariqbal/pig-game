73960457eca1255af37b83d8c81b22aa3350ed36
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-29 20:51:15 +0600

    Rolling the dice
    current score
    score
    
    getElementById used

--- app.js
+++ app.js
@@ -10,22 +10,67 @@ GAME RULES:
 */
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 scores = [0,0];
 roundScore = 0;
 activePlayer =1;
 
-dice =  Math.floor(Math.random()*6)+1;
-
-//setter
-document.querySelector('#current-'+ activePlayer).textContent = dice;
-// document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>'; 
 
 
-//getter //read from html
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+
 
 //initial dice
 
 document.querySelector('.dice').style.display = 'none';
+
+//getelementby id   only works for id   faster than query selector
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+//Event handler
+//callback function
+//document.querySelector('.btn-roll').addEventListener('click', btn ); //btn callback function
+
+
+//anonymus function dosent hav e name cant be re use
+// document.querySelector('.btn-roll').addEventListener('click', function(){  //here function => anonymus function
+	
+// } );
+
+
+
+// function btn(){
+
+// }
+// btn();
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+	//1. Random Number
+	var dice =  Math.floor(Math.random()*6)+1;
+
+	//2. Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-'+ dice + '.png';
+	//3. Update the round score if the rolled number was not a 1
+
+} );
+
+
+
+
+//setter
+//document.querySelector('#current-'+ activePlayer).textContent = dice;
+// document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice + '</em>'; 
+
+
+//getter //read from html
+//var x = document.querySelector('#score-0').textContent;
