60f4eaaf68517f3583877c6747a9808161fdd6a0
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-03-30 11:47:57 +0600

    Creating a Game Initialization Function

--- app.js
+++ app.js
@@ -11,24 +11,14 @@ GAME RULES:
 
 
 var scores, roundScore, activePlayer;
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 
 
 
 
-//initial dice
-document.querySelector('.dice').style.display = 'none';
 
 
-//getElementById only works for id   *faster than querySelector*
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 
 
@@ -67,7 +57,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 } );
 
-//*******************************Implementing Our 'Hold' Function and the DRY Principle********************************//
+
 document.querySelector('.btn-hold').addEventListener('click', function(){ 
 	//Add CURRENT score to GLOBAL score
 	scores[activePlayer] += roundScore;
@@ -103,6 +93,39 @@ function nextPlayer(){
       document.querySelector('.dice').style.display = 'none';
 }
 
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;	
+
+
+
+    //initial dice
+document.querySelector('.dice').style.display = 'none';
+
+
+//getElementById only works for id   *faster than querySelector*
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+
+document.querySelector('.player-0-panel').classList.add('active');
+
+}
 
 
 
